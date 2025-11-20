b608d660acf83ac16f4fad90a3a8bb6540e9fd58
maksim17121 <maksim17121@gmail.com>
2025-11-20 07:52:55 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    float x, y;
+
+    std::cout << "x: ";
+    std::cin >> x;
+    std::cout << "y: ";
+    std::cin >> y;
+
+    float R = 1.0 / cos((y * y) / x);
+
+    float S = pow(x + log(1 / x), 1 / 3);
+
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+
+    float C = (R > S) ? R : S;
+
+    std::cout << "C = " << C << std::endl;
+
+    return 0;
+}
