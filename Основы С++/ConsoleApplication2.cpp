541649e0892cfbb1b1819d5721930b47dfb17e0a
maksim17121 <maksim17121@gmail.com>
2025-11-20 08:52:02 +0300

    Add files via upload

---
+++ Основы С++/ConsoleApplication2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    float x, y;
+
+    std::cout << "x: ";
+    std::cin >> x;
+    std::cout << "y: ";
+    std::cin >> y;
+
+    float R = 1 / cos((y * y) / x);
+
+    float S = cbrt(x + log(1 / x));
+
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+
+    float C = std::max(R, S);
+
+    std::cout << "C = " << C << std::endl;
+
+    std::cout << "..";
+    std::cin.ignore();
+    std::cin.get();
+
+    return 0;
+}
